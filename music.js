cd4399be17b2b0b663dc7ab1777eb495f8f85060
shashank1947 <72707580+shashank1947@users.noreply.github.com>
2021-05-10 18:40:13 +0530

    CHANES

---
+++ music.js
@@ -0,0 +1,238 @@
+var aud=document.getElementById("mus");
+var audb=document.getElementById("musb");
+var audc=document.getElementById("musc");
+var audd=document.getElementById("musd");
+var aude=document.getElementById("muse");
+var audf=document.getElementById("musf");
+var audg=document.getElementById("musg");
+var audh=document.getElementById("mush");
+var audi=document.getElementById("musi");
+var audj=document.getElementById("musj");
+var audk=document.getElementById("musk");
+var audl=document.getElementById("musl");
+var audm=document.getElementById("musm");
+var audn=document.getElementById("musn");
+var audo=document.getElementById("muso");
+var audp=document.getElementById("musp");
+var audq=document.getElementById("musq");
+var audr=document.getElementById("musr");
+var auds=document.getElementById("muss");
+var audt=document.getElementById("must");
+var audu=document.getElementById("musu");
+var audv=document.getElementById("musv");
+var audw=document.getElementById("musw");
+var audx=document.getElementById("musx");
+var audy=document.getElementById("musy");
+var audz=document.getElementById("musz");
+
+
+
+function musicplay()
+{
+    aud.play();
+} 
+
+function next()
+{
+    aud.pause();
+}
+function songplay()
+{
+    audb.play();
+} 
+function songnext()
+{
+    audb.pause();
+}
+function cletterplay()
+{
+    audc.play();
+} 
+function cletternext()
+{
+    audc.pause();
+}
+function dletterplay()
+{
+    audd.play();
+} 
+function dletternext()
+{
+    audd.pause();
+}
+function eletterplay()
+{
+    aude.play();
+} 
+function eletternext()
+{
+    aude.pause();
+}
+function fletterplay()
+{
+    audf.play();
+} 
+function fletternext()
+{
+    audf.pause();
+}
+function gletterplay()
+{
+    audg.play();
+} 
+function gletternext()
+{
+    audg.pause();
+}
+function hletterplay()
+{
+    audh.play();
+} 
+function hletternext()
+{
+    audh.pause();
+}
+function iletterplay()
+{
+    audi.play();
+} 
+function iletternext()
+{
+    audi.pause();
+}
+function jletterplay()
+{
+    audj.play();
+} 
+function jletternext()
+{
+    audj.pause();
+}
+function kletterplay()
+{
+    audk.play();
+} 
+function kletternext()
+{
+    audk.pause();
+}
+function lletterplay()
+{
+    audl.play();
+} 
+function lletternext()
+{
+    audl.pause();
+}
+function mletterplay()
+{
+    audm.play();
+} 
+function mletternext()
+{
+    audm.pause();
+}
+function nletterplay()
+{
+    audn.play();
+} 
+function nletternext()
+{
+    audn.pause();
+}
+function oletterplay()
+{
+    audo.play();
+} 
+function oletternext()
+{
+    audo.pause();
+}
+function pletterplay()
+{
+    audp.play();
+} 
+function pletternext()
+{
+    audp.pause();
+}
+function qletterplay()
+{
+    audq.play();
+} 
+function qletternext()
+{
+    audq.pause();
+}
+function rletterplay()
+{
+    audr.play();
+} 
+function rletternext()
+{
+    audr.pause();
+}
+function sletterplay()
+{
+    auds.play();
+} 
+function sletternext()
+{
+    auds.pause();
+}
+function tletterplay()
+{
+    audt.play();
+} 
+function tletternext()
+{
+    audt.pause();
+}
+function uletterplay()
+{
+    audu.play();
+} 
+function uletternext()
+{
+    audu.pause();
+}
+function vletterplay()
+{
+    audv.play();
+} 
+function vletternext()
+{
+    audv.pause();
+}
+function wletterplay()
+{
+    audw.play();
+} 
+function wletternext()
+{
+    audw.pause();
+}
+function xletterplay()
+{
+    audx.play();
+} 
+function xletternext()
+{
+    audx.pause();
+}
+function yletterplay()
+{
+    audy.play();
+} 
+function yletternext()
+{
+    audy.pause();
+}
+function zletterplay()
+{
+    audz.play();
+} 
+function zletternext()
+{
+    audz.pause();
+}
